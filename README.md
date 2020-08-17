4ff55c6f6a40bd15b6d87651707ddaca5e8e9732
Vance <coletain@gmail.com>
2020-08-16 20:47:54 -0700

    Adds detection for modules using !important and switches to local storage method

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Allows users to resize the sidebar of Foundry VTT
 
 Simply hover over the edge of the sidebar and a resize cursor should appear, drag to the size you prefer. Will remember the size, individual to the computer you are using.
+
+# Module & System Incompatibilities
+Certain modules or systems may be incompatible with this module if they specify an !important flag on the width of the sidebar, in such cases please ping me on discord @vance#1935 and I can look into adding compatibility.
--- vance-sidebar-resizer.js
+++ vance-sidebar-resizer.js
@@ -1,65 +1,71 @@
 Hooks.on("ready", function() {
-    // Setup vars
-    const minSize = 300;
-    const sidebar = document.querySelector('#sidebar');
-    const initSize = getCookie('vance-sidebar-size');
-    let mouseStart, startSize, newSize;
-    
-    // Reset saved width if it exists
-    // Also renew the cookie so it doesn't eventually expire
-    if(Number.isInteger(+initSize)) {
-        sidebar.style.width = `${initSize}px`;
-        setCookie('vance-sidebar-size', initSize, 365);
+  // Setup vars
+  let important = '';
+  let minSize = 300;
+  const sidebar = document.querySelector('#sidebar');
+  const initSize = window.localStorage.getItem('vance-sidebar-size');
+  let mouseStart, startSize, newSize;
+
+  // List of known incompatibilities that need !important flag
+  let compatibility = [
+    {
+        type: 'module',
+        name: 'pathfinder-ui',
+        min: 350,
+        important: true,
+    },
+  ]
+  
+  // Check if any incompatibilities are active
+  compatibility.forEach((i) => {
+    if (i.type === 'module') {
+      if (game.modules.get(i.name)?.active) {
+        if (i.important) important = ' !important';
+        minSize = i.min;
+      }
     }
+  });
 
-    // Create a resizer handle
-    const resizer = document.createElement('div');
-    resizer.style.width = '6px';
-    resizer.style.height = '100%';
-    resizer.style.position = 'absolute';
-    resizer.style.left = 0;
-    resizer.style.top = 0;
-    resizer.style.cursor = 'col-resize';
-    sidebar.appendChild(resizer);
+  // Reset saved width if it exists
+  if (Number.isInteger(+initSize)) {
+    sidebar.setAttribute('style', `width: ${initSize}px${important}`);
+  }
 
-    // Listen for mousedown on resizer
-    resizer.addEventListener('mousedown', startResize, false);
+  // Create a resizer handle
+  const resizer = document.createElement('div');
+  resizer.style.width = '6px';
+  resizer.style.height = '100%';
+  resizer.style.position = 'absolute';
+  resizer.style.left = 0;
+  resizer.style.top = 0;
+  resizer.style.cursor = 'col-resize';
+  sidebar.appendChild(resizer);
 
-    // React to user resizing
-    function startResize(e) {
-        mouseStart = e.clientX;
-        startSize = sidebar.offsetWidth;
-        window.addEventListener('mousemove', resize, false);
-        window.addEventListener('mouseup', stopResize, false);
+  // Listen for mousedown on resizer
+  resizer.addEventListener('mousedown', startResize, false);
+
+  // React to user resizing
+  function startResize(e) {
+    mouseStart = e.clientX;
+    startSize = sidebar.offsetWidth;
+    window.addEventListener('mousemove', resize, false);
+    window.addEventListener('mouseup', stopResize, false);
+  }
+
+  // Perform the resize operation
+  function resize(e) {
+    newSize = Math.round(startSize + mouseStart - e.clientX);
+    if (newSize >= minSize) {
+      sidebar.setAttribute('style', `width: ${newSize}px${important}`);
+    } else {
+      sidebar.setAttribute('style', `width: ${minSize}px${important}`);
     }
+  }
 
-    // Perform the resize operation
-    function resize(e) {
-        newSize = startSize + mouseStart - e.clientX;
-        if (newSize >= minSize) {
-            sidebar.style.width = `${newSize}px`;
-        } else {
-            sidebar.style.width = `${minSize}px`;
-        }
-    }
-
-    // On mouseup remove listeners & save final size
-    function stopResize(e) {
-        setCookie('vance-sidebar-size', sidebar.offsetWidth, 365);
-        window.removeEventListener('mousemove', resize, false);
-        window.removeEventListener('mouseup', stopResize, false);
-    }
-
+  // On mouseup remove listeners & save final size
+  function stopResize(e) {
+  window.localStorage.setItem('vance-sidebar-size', sidebar.offsetWidth);
+  window.removeEventListener('mousemove', resize, false);
+  window.removeEventListener('mouseup', stopResize, false);
+  }
 });
-
-// Gets cookie value by name
-function getCookie(name) {
-    var v = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return v ? v[2] : null;
-}
-// Sets a cookie
-function setCookie(name, value, days) {
-    var d = new Date;
-    d.setTime(d.getTime() + 24*60*60*1000*days);
-    document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
-}
